577e2c25a59e157ab9d098bc87fda5bf66173d0b
Zhang Lingyun <zhang.lingyun@pwc.com>
2019-12-11 22:02:36 +0800

    init commit

---
+++ src/main/java/com/example/zirauser/ZiraUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.zirauser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZiraUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZiraUserApplication.class, args);
+	}
+
+}
